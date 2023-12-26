3e02c7eead93041a48089244759e18a4abcc8e8e
gabrielvtdev <zgabrielp3@gmail.com>
2023-12-26 17:18:31 -0300

    user button add

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return(
-    <div className="flex items-center justify-center h-full w-full">
+    <div className="flex items-center justify-center h-full w-full p-40">
       {children}
     </div>
   )
