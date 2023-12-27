4b93aebcd179b822d2b5a98bb54b91dc1d138849
gabrielvtdev <zgabrielp3@gmail.com>
2023-12-27 10:42:21 -0300

    api created and add some alerts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider} from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
 
 import './globals.css'
 
@@ -17,11 +18,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
   return (
     <ClerkProvider>
       <html lang="pt-BR">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider/>
           {children}
         </body>
